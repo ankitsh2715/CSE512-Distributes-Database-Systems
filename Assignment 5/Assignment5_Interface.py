00df0d6575cd43ca5eff8c96a55b5caaecf5c316
Ankit Sharma <ankitsh2715@gmail.com>
2021-04-18 04:31:26 -0700

    folder restructure

---
+++ Assignment 5/Assignment5_Interface.py
@@ -0,0 +1,15 @@
+#
+# Assignment5 Interface
+# Name: 
+#
+
+from pymongo import MongoClient
+import os
+import sys
+import json
+
+def FindBusinessBasedOnCity(cityToSearch, saveLocation1, collection):
+    pass
+
+def FindBusinessBasedOnLocation(categoriesToSearch, myLocation, maxDistance, saveLocation2, collection):
+    pass
